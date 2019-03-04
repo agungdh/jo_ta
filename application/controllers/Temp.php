2440ecfb168d7f5ea95cfc25795bfa266890fb62
AgungDH <agungdh@live.com>
2019-03-04 19:09:57 +0700

    update

--- application/controllers/Temp.php
+++ application/controllers/Temp.php
@@ -1,22 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-use agungdh\Pustaka;
-
 class Temp extends CI_Controller {
-	public $pustaka;
-
-	public function __construct()
-	{
-		parent::__construct();
-		
-		$this->pustaka = new Pustaka();
-	}
 
 	public function index()
 	{
-		// echo $this->pustaka;
-		// echo $this->pustaka->rupiah(100000);
-		$this->load->view('template/template');
+		echo $this->helper->rupiah(20000);
 	}
 }
---
+++ application/libraries/helper.php
@@ -0,0 +1,6 @@
+<?php 
+ 
+class helper extends \agungdh\Pustaka {
+ 
+
+}
